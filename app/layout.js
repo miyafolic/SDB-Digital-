fa9235091a08ace035cda60577b491403d4e849d
miyafolic <miyafolic@pm.me>
2026-07-25 15:59:06 +0530

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "SDB DIGITAL",
+  description: "Watch Ads & Earn Real Cash",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-950 text-slate-100 min-h-screen flex justify-center">
+        <div className="w-full max-w-md bg-slate-900 min-h-screen border-x border-slate-800 relative pb-20 shadow-2xl">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
