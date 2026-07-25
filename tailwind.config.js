3f95383f47f38aa5f3f8f017cfd916f1d9d75cc2
miyafolic <miyafolic@pm.me>
2026-07-25 16:41:03 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          blue: "#00C8FF",
-          green: "#10B981",
-          dark: "#0F172A",
-          card: "#1E293B",
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
